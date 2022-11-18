c87736fc59815f76547bc14cd54ac3cea539e431
jnesrsta-cen61181 <89904426+jnesrsta-cen61181@users.noreply.github.com>
2022-11-18 23:27:34 +0100

    Added rows

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(10+10);
 
@@ -6,3 +7,104 @@ console.log(10);
 
 let firstName = "George";
 console.log(firstName);
+
+// example of a variable with reserved keyword
+let $new = "George";
+let _function = "George";
+
+// constant shloud be in uppercase
+let PI = 3.1415; 
+
+// Example of better variable names
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
+
+// Assignments 1
+let country = "USA"
+let continent = "North America"
+let population = 300000000
+/*
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Types
+/* Primitive Data Types
+    Number
+    String
+    Boolean
+    Undefined (empty value for to be fiiled in later)
+    Null (empty value)
+    Symbol (ES6/ES2015)
+    BigInt (ES2020)
+
+
+
+console.log(true);
+console.log(false);
+
+console.log(typeof true);
+
+let javaScriptIsFun = true;
+
+console.log(typeof javaScriptIsFun);
+
+if (javaScriptIsFun) {
+    console.log("JavaScript is fun!");
+}
+
+javaScriptIsFun = "Yes!";
+
+console.log(javaScriptIsFun);
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); // returns object instead of null
+
+/*
+LECTURE: Data Types
+1. Declareavariablecalled'isIsland'andsetitsvalueaccordingtoyour country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+2. Logthetypesof'isIsland','population','country'and'language' to the console
+*/
+/*
+let isIsland = false;
+console.log(isIsland);
+console.log(typeof isIsland);
+
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+let age = 30;
+console.log(age);
+console.log(typeof age);
+
+age = 31;
+console.log(age);
+console.log(typeof age);
+
+age = 'Hello';
+console.log(age);
+console.log(typeof age);
+
+*/
+const birthYear = 1991;
+console.log(birthYear);
+console.log(typeof birthYear);
+
+//birthYear = "Hello"; // will not work because birthYear is a constant defined as number before
+
+
+var testAge = 40; // old way of declaring variables (not used anymore) 
+console.log(testAge);
+console.log(typeof testAge);
+testAge = 31;
+
+testName = "George"; // will work but not recommended because it will create a global variable
+
